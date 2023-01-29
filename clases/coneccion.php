2b24b50071718322f96e752ca33f825349e93f98
Moises Jaramillo <mjaramillo314@gmail.com>
2023-01-29 05:59:15 -0400

    coneccion estandar

--- clases/coneccion.php
+++ clases/coneccion.php
@@ -4,7 +4,7 @@
       var $nombre;
       public static function con()
       {
-         $con=mysql_connect('localhost', 'root', 'root');
+         $con=mysql_connect('localhost', 'usuario', 'clave');
          mysql_select_db('sices');
          return $con;
       }
